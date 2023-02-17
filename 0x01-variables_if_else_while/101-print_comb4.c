1f5181e6b537ba186fb5a0812b049d9f1709ed5d
awuku_k <kelvinawuku402@gmail.com>
2023-02-17 04:27:16 -0800

    Quick Fix

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,30 +1,34 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
- * main - Determine if the last digit of a random number is
- * greater than or less than 5, or is zero..
-(*
- * Return: 0 on success
+ * main - Prints all possible combinations of three different digits,
+ *        in ascending order, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
  */
-#include <stdio.h>
+int main(void)
+{
+	int digit1, digit2, digit3;
 
-int main() {
-    for (int i = 0; i <= 2; i++) { 
-        for (int j = i+1; j <= 3; j++) { 
-            for (int k = j+1; k <= 4; k++) { 
-                int code = i*100 + j*10 + k; 
-                if (code != 123 && code != 132 && code != 213 && code != 231 && code != 312 && code != 321) {
-                    putchar('0' + i);
-                    putchar('0' + j); 
-                    putchar('0' + k); 
-                    if (code != 420) { 
-                        putchar(', ');
-                    }
-                }
-            }
-        }
-    }
-    putchar('\n');
-    return 0;
+	for (digit1 = 0; digit1 < 8; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
+		{
+			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+			{
+				putchar((digit1 % 10) + '0');
+				putchar((digit2 % 10) + '0');
+				putchar((digit3 % 10) + '0');
+
+				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+					continue;
+				
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
 }
